4c64a63201832ee498a8ac20fe0f7a4f80640b27
Logan Greenawalt <logan.greenawalt@icloud.com>
2022-01-08 15:52:36 -0500

    sort a done

--- sorta.ts
+++ sorta.ts
@@ -1,5 +1,18 @@
 import { get_array } from "./data.ts";
 export async function sort() {
-  let list: number[] = await get_array();
-  console.log(list);
+  const arr: number[] = await get_array();
+  console.log(arr);
+
+  for (let index = 1; index < arr.length; index++) {
+    const current = arr[index];
+    let pi = index - 1;
+
+    while (pi >= 0 && arr[pi] > current) {
+      arr[pi + 1] = arr[pi];
+      pi = pi - 1;
+    }
+    arr[pi + 1] = current;
+
+    console.log(arr);
+  }
 }
