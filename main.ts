93ddede47849bbcceb9f86616430471daf6123ee
Joe Greenawalt <subs@closet6.com>
2022-01-09 12:32:05 -0500

    pretty print

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { sort as sorta } from "./sorta.ts";
 import { sort as sortb } from "./sortb.ts";
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { get_array } from "./data.ts";
-import { run_record } from "./types.ts";
+import { run_log, run_record } from "./types.ts";
 
 const DATA_SOURCE = "./ztest_data.csv";
 const flags = parse(Deno.args);
@@ -12,8 +12,13 @@ const run_times = flags.n || 1;
 const run_records: run_record[] = [];
 
 for (let idx = 1; idx <= run_times; idx++) {
-  let run_log = sorta(await get_array(DATA_SOURCE));
-  let rr: run_record = {
+  let arr: number[],
+    run_log: run_log,
+    rr: run_record;
+
+  arr = await get_array(DATA_SOURCE);
+  run_log = sorta(arr);
+  rr = {
     sort: "insertion",
     run_time: run_log.run_time,
     loop_count: run_log.loop_count,
@@ -21,8 +26,11 @@ for (let idx = 1; idx <= run_times; idx++) {
     array_size: run_log.array_size,
   };
 
+  print_run(arr, rr);
+
+  arr = await get_array(DATA_SOURCE);
   run_records.push(rr);
-  run_log = sortb(await get_array(DATA_SOURCE));
+  run_log = sortb(arr);
   rr = {
     sort: "selection",
     run_time: run_log.run_time,
@@ -32,6 +40,7 @@ for (let idx = 1; idx <= run_times; idx++) {
   };
 
   run_records.push(rr);
+  print_run(arr, rr);
 }
 
 let str =
@@ -42,3 +51,17 @@ for (const rr of run_records) {
     `${rr.run_number},"${rr.sort}",${rr.array_size},${rr.run_time},${rr.loop_count}\n`;
 }
 Deno.writeTextFileSync(`zoutput-${Date.now()}.csv`, str);
+
+function print_run(arr: number[], rr: run_record) {
+  console.log(
+    `
+    Performance: 
+      run number: ${rr.run_number}
+      sort type: ${rr.sort}
+      array length: ${rr.array_size}
+      execution time: ${rr.run_time}
+      loop count: ${rr.loop_count}\n
+    Result: ${arr}
+    `,
+  );
+}
